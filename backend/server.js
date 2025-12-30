23a2095fef9d7be77b7120042ad98e15ac549a45
Ashutosh Ranjan <kumarashu493@gmail.com>
2025-12-31 00:42:29 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://b2b-1-e2v4.onrender.com',
     credentials: true,
   })
 );
